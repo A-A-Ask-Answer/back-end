d9b9ce078dc044d37fa46adc6c7ce6634fe43075
cousim46 <wjdghldns24@naver.com>
2023-01-24 16:33:02 +0900

    project settig

---
+++ src/test/java/a_a/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package a_a.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
