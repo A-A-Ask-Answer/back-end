9e9098968ccd2f1e6657ec57337f1ad68a9f788a
cousim46 <wjdghldns24@naver.com>
2023-01-30 08:31:09 +0900

    Add : 스프링 시큐리티에서 지원해주는 암호화 라이브러리 빈 설정 삭제

--- src/main/java/a_a/project/ProjectApplication.java
+++ src/main/java/a_a/project/ProjectApplication.java
@@ -2,19 +2,14 @@ package a_a.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+@EnableRedisHttpSession
 public class ProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
-    }
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 
 
