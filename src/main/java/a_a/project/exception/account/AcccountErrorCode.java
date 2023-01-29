69b9f53d6887a32721bae659775ac5c941d6556a
cousim46 <wjdghldns24@naver.com>
2023-01-30 08:27:58 +0900

    Add : 유저에 대한 커스텀 예외 추가

--- src/main/java/a_a/project/exception/account/AcccountErrorCode.java
+++ src/main/java/a_a/project/exception/account/AcccountErrorCode.java
@@ -1,4 +1,4 @@
-package a_a.project.exception.user;
+package a_a.project.exception.account;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -6,13 +6,15 @@ import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
 @Getter
-public enum UserErrorCode {
+public enum AcccountErrorCode {
 
     DUPLICATE_LOGIN_ID(HttpStatus.CONFLICT, "중복된 아이디입니다."),
 
     DUPLICATE_KEYWORD(HttpStatus.CONFLICT, "중복된 키워드입니다."),
 
-    NOT_EQUALS_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호와 비밀번호 재확인이 일치하지 않습니다.");
+
+    NOT_EQUALS_PASSWORD_CONFIRM_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호와 비밀번호 재확인이 일치하지 않습니다."),
+    NOT_EQUALS_LOGIN_ID_OR_PASSWORD(HttpStatus.BAD_REQUEST, "아이디 또는 비밀번호를 잘못입력했습니다.");
 
 
     private final HttpStatus status;
