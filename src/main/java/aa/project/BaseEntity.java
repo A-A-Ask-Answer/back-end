8fba2b0bede8af085c64bad5d4be7f7a9fee9823
cousim46 <wjdghldns24@naver.com>
2023-02-01 20:32:44 +0900

    Fix : 기본키 AuditingEntity로 이동

--- src/main/java/aa/project/BaseEntity.java
+++ src/main/java/aa/project/BaseEntity.java
@@ -5,14 +5,17 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 @Getter
 public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
 
     @CreatedDate
     private LocalDateTime createdAt;
