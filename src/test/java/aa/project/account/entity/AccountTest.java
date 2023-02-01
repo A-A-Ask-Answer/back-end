c59ffa89adb6fe063c15b6f4df3f33e51b26e4ee
cousim46 <wjdghldns24@naver.com>
2023-02-01 20:33:58 +0900

    Add : Account entity에 대한 값 바인딩 TestCode

---
+++ src/test/java/aa/project/account/entity/AccountTest.java
@@ -0,0 +1,27 @@
+package aa.project.account.entity;
+
+import aa.project.account.entity.type.Gender;
+import aa.project.account.entity.type.Role;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AccountTest {
+
+
+    @Test
+    @DisplayName("Account 생성자에 값 바인딩하면 유저 권한 USER와 삭제여부 false 반환")
+    void accountSave() {
+        Account account = new Account("개발자", "test", "1234", "hoestory", Gender.MALE);
+
+        assertThat(account.getPassword()).isEqualTo("1234");
+        assertThat(account.getKeyWord()).isEqualTo("개발자");
+        assertThat(account.getName()).isEqualTo("hoestory");
+        assertThat(account.getLoginId()).isEqualTo("test");
+        assertThat(account.getGender()).isEqualTo(Gender.MALE);
+        assertThat(account.isActive()).isFalse();
+        assertThat(account.getRole()).isEqualTo(Role.USER);
+    }
+
+}
