4a4c545e11fe021e4f2027445336d9d74994ae3d
cousim46 <wjdghldns24@naver.com>
2023-02-01 20:34:13 +0900

    Fix : 제약 조건 추가

--- src/main/java/aa/project/account/entity/Account.java
+++ src/main/java/aa/project/account/entity/Account.java
@@ -3,46 +3,53 @@ package aa.project.account.entity;
 import aa.project.BaseEntity;
 import aa.project.account.entity.type.Gender;
 import aa.project.account.entity.type.Role;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 
 @Entity
 @Getter
 @NoArgsConstructor
-
+@AllArgsConstructor
 public class Account extends BaseEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String keyWord;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String loginId;
 
+    @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false)
     @Enumerated(value = EnumType.STRING)
     private Role role;
+
+    @Column(nullable = false)
     @Enumerated(value = EnumType.STRING)
     private Gender gender;
 
+    @Column(nullable = false)
+    @ColumnDefault("false")
     private boolean isActive; // 탈퇴여부
 
-    public Account(String keyWord, String loginId, String password, String name) {
+    public Account(String keyWord, String loginId, String password, String name, Gender gender) {
         this.keyWord = keyWord;
         this.loginId = loginId;
         this.password = password;
         this.name = name;
+        this.gender = gender;
         this.role = Role.USER;
     }
 
-    public void gender(Gender gender) {
-        this.gender = gender;
-    }
 }
