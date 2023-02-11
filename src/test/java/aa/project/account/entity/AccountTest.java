d98ea22f1b33ebbf44e9056ddf7231134e2d873f
cousim46 <wjdghldns24@naver.com>
2023-02-11 23:14:04 +0900

    Fix : 공백 제거 및 권한 설정

--- src/test/java/aa/project/account/entity/AccountTest.java
+++ src/test/java/aa/project/account/entity/AccountTest.java
@@ -8,12 +8,11 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class AccountTest {
-
-
     @Test
     @DisplayName("Account 생성자에 값 바인딩하면 유저 권한 USER와 삭제여부 false 반환")
     void accountSave() {
         Account account = new Account("개발자", "test", "1234", "hoestory", Gender.MALE);
+        account.createdAccountRole(Role.USER);
 
         assertThat(account.getPassword()).isEqualTo("1234");
         assertThat(account.getKeyWord()).isEqualTo("개발자");
