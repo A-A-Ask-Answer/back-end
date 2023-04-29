262d70582d196be2903f832599812eba524db806
cousim46 <wjdghldns24@naver.com>
2023-04-30 04:03:07 +0900

    비밀번호 암호화를 위한 외부 라이브러리 빈으로 등록

--- src/main/java/aa/project/ProjectApplication.java
+++ src/main/java/aa/project/ProjectApplication.java
@@ -2,7 +2,9 @@ package aa.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
@@ -12,6 +14,11 @@ public class ProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 
