fd68bdd2aa32944e78e52adbda5da127cf17f4e3
cousim46 <wjdghldns24@naver.com>
2023-04-30 04:02:02 +0900

    파일명 변경

--- src/main/java/aa/project/ExceptionHandlerControllerAdvice.java
+++ src/main/java/aa/project/ExceptionHandlerControllerAdvice.java
@@ -1,13 +1,14 @@
 package aa.project;
 
-import aa.project.exception.account.AccountException;
-import aa.project.exception.account.AccountExceptionResponseEntity;
+import aa.project.exception.account.CustomException;
+import aa.project.exception.account.CustomExceptionResponseEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.validation.ConstraintViolationException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,17 +16,23 @@ import java.util.Map;
 public class ExceptionHandlerControllerAdvice {
 
 
-    @ExceptionHandler(AccountException.class)
-    public ResponseEntity<AccountExceptionResponseEntity> toAccountExceptionResponse(AccountException accountException) {
-        return AccountExceptionResponseEntity.toAccountExceptionResponse(accountException.getAcccountErrorCode());
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<CustomExceptionResponseEntity> toAccountExceptionResponse(CustomException customException) {
+        return CustomExceptionResponseEntity.toAccountExceptionResponse(customException.getErrorCode());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> responseValidation(MethodArgumentNotValidException e) {
+    public ResponseEntity<Map<String, String>> responseRequestBodyValidation(MethodArgumentNotValidException e) {
+        System.out.println("asdasdasd");
         Map<String, String> error = new HashMap<>();
         e.getBindingResult().getAllErrors()
                 .forEach(c -> error.put(((FieldError) c).getField(), c.getDefaultMessage()));
         return ResponseEntity.badRequest().body(error);
+    }
+
+    @ExceptionHandler(value = ConstraintViolationException.class)
+    public ResponseEntity<String> responseRequestParamValidation(ConstraintViolationException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
     }
 
 
